7089fac447e56b6fd95bc1c3f9a7bd14c54ae493
nxbckns <mshare7000@gmail.com>
2026-05-26 05:24:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,13 @@
 import discord
-from discord.ext import commands
-import yt_dlp
 import os
-from threading import Thread
-from http.server import HTTPServer, BaseHTTPRequestHandler
-
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b"Bot is alive!")
-    def log_message(self, format, *args):
-        pass
-
-def run_server():
-    server = HTTPServer(('0.0.0.0', 8080), Handler)
-    server.serve_forever()
-
-Thread(target=run_server, daemon=True).start()
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix="!", intents=intents)
+client = discord.Client(intents=intents)
 
-@bot.event
+@client.event
 async def on_ready():
-    print("Bot is ready!")
+    print(f"Logged in as {client.user}")
 
-@bot.event
-async def on_message(message):
-    if message.author.bot:
-        return
-    await bot.process_commands(message)
-
-def search_youtube(query):
-    ydl_opts = {"format": "bestaudio", "noplaylist": True, "quiet": True}
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info("ytsearch:" + query, download=False)
-        return info["entries"][0]
-
-@bot.command(name="p")
-async def play(ctx, *, query):
-    if not ctx.author.voice:
-        return await ctx.send("Join a voice channel first!")
-    await ctx.send("Searching...")
-    info = search_youtube(query)
-    url = info["url"]
-    title
+token = os.environ.get("DISCORD_TOKEN")
+print(f"Token exists: {token is not None}")
+client.run(token)
